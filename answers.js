0a3ead5edbefec9553ea53b03f2a1e89e686517f
Annie <annie-d@hotmail.ca>
2016-04-13 17:22:40 +0000

    created a function that returns the first character of a string

--- answers.js
+++ answers.js
@@ -1,1 +1,6 @@
-// comment
+// Write a function that takes a string and returns the first character of the string. 
+// Test your function on a few inputs, including the empty string.
+
+function firstCharacter(string) {
+    return string[0];
+}
