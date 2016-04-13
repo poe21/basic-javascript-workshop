05b7811c945d83a54e9648b1172a86a6609b1e9c
Annie <annie-d@hotmail.ca>
2016-04-13 17:39:14 +0000

    added a second function to get the first character

--- answers.js
+++ answers.js
@@ -4,3 +4,14 @@
 function firstCharacter(string) {
     return string[0];
 }
+
+console.log(firstCharacter("Cat"));
+console.log(firstCharacter(""));
+
+// Alernative function to get a first character as requested by Codrin
+
+function firstCharacterAlternate(str) {
+    return str.substr(0,1);
+}
+
+console.log(firstCharacterAlternate("Annie"));
